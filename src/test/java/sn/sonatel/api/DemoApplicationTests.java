d3e8bc578e83de5adf2d2fe938a1de7c22bbeae3
Mandiaye DIOP <mandiaye.diop@orange-sonatel.com>
2022-10-29 15:08:47 +0000

    Correct Unit tests

--- src/test/java/sn/sonatel/api/DemoApplicationTests.java
+++ src/test/java/sn/sonatel/api/DemoApplicationTests.java
@@ -2,9 +2,10 @@ package sn.sonatel.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import sn.sonatel.api.config.SecurityConfiguration;
 
-@SpringBootTest
-class SonatelApiSpringbootApplicationTests {
+@SpringBootTest(classes = SecurityConfiguration.class)
+class DemoApplicationTests {
 
     @Test
     void contextLoads() {
